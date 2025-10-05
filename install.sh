a98d4eecfc82081d9650f91bcfe0da3e94d61198
Decho Kocharin <sourcedecho@outlook.com>
2025-10-05 22:56:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -416,7 +416,7 @@
     # Compile Blocknotify
     echo -e "Compiling Blocknotify"
     cd ~
-    sudo git clone https://github.com/msy2008/yiimp.git
+    sudo git clone https://github.com/Kudaraidee/yiimp.git
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     make -j$((`nproc`+1))
@@ -1030,27 +1030,24 @@
     cd yiimp/sql
 
     # Import sql dump
-    sudo zcat 2016-04-03-yaamp.sql.gz | sudo mysql --defaults-group-suffix=host1 --defaults-file=/home/${whoami}/.my.cnf
+    sudo zcat 2024-03-06-complete_export.sql.gz | sudo mysql --defaults-group-suffix=host1 --defaults-file=/home/${whoami}/.my.cnf
     echo -e "$GREEN Done...$COL_RESET"
 
     # Oh the humanity!
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-04-24-market_history.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-04-27-settings.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-05-11-coins.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-05-15-benchmarks.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-05-23-bookmarks.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-06-01-notifications.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-06-04-bench_chips.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2016-11-23-coins.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-02-05-benchmarks.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-03-31-earnings_index.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-05-accounts_case_swaptime.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-06-payouts_coinid_memo.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-09-notifications.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-10-bookmarks.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2017-11-segwit.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2018-01-stratums_ports.sql
-    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2018-02-coins_getinfo.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-03-18-add_aurum_algo.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-03-29-add_github_version.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-03-31-add_payout_threshold.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-04-01-add_auto_exchange.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-04-01-shares_blocknumber.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-04-05-algos_port_color.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-04-22-add_equihash_algos.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-04-23-add_pers_string.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-04-29-add_sellthreshold.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2024-05-04-add_neoscrypt_xaya_algo.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2025-02-06-add_usemweb.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2025-02-13-add_xelisv2-pepew.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2025-02-23-add_algo_kawpow.sql
+    sudo mysql --defaults-group-suffix=host1 yiimpfrontend --force < 2025-10-05-add_argon2d1000.sql
     echo -e "$GREEN Done...$COL_RESET"
 
     
