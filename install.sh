09296f27bcdc4212d556d549af0509291253f5d5
Decho Kocharin <sourcedecho@outlook.com>
2025-10-06 18:46:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1003,33 +1003,24 @@
     define('"'"'YIIMP_MYSQLDUMP_PATH'"'"', '"'"''"/var/yiimp/sauv"''"'"');
     /* Keys required to create/cancel orders and access your balances/deposit addresses */
 
-    define('"'"'EXCH_BINANCE_KEY'"'"', '"'"''"'"');
     define('"'"'EXCH_BINANCE_SECRET'"'"', '"'"''"'"');
 
     define('"'"'EXCH_CEXIO_SECRET'"'"', '"'"''"'"');
 
-    define('"'"'EXCH_EXBITRON_KEY'"'"', '"'"''"'"');
-
     define('"'"'EXCH_HITBTC_SECRET'"'"', '"'"''"'"');
-    define('"'"'EXCH_HITBTC_KEY'"'"', '"'"''"'"');
 
-    define('"'"'EXCH_KRAKEN_KEY'"'"', '"'"''"'"');
     define('"'"'EXCH_KRAKEN_SECRET'"'"', '"'"''"'"');
 
     define('"'"'EXCH_KUCOIN_SECRET'"'"', '"'"''"'"');
 
-    define('"'"'EXCH_POLONIEX_KEY'"'"', '"'"''"'"');
     define('"'"'EXCH_POLONIEX_SECRET'"'"', '"'"''"'"');
 
-    define('"'"'EXCH_SAFETRADE_KEY'"'"', '"'"''"'"');
     define('"'"'EXCH_SAFETRADE_SECRET'"'"', '"'"''"'"');
 
     define('"'"'EXCH_TRADEOGRE_SECRET'"'"', '"'"''"'"');
 
-    define('"'"'EXCH_YOBIT_KEY'"'"', '"'"''"'"');
     define('"'"'EXCH_YOBIT_SECRET'"'"', '"'"''"'"');
 
-    define('"'"'EXCH_NESTEX_KEY'"'"', '"'"''"'"');
     define('"'"'EXCH_NESTEX_SECRET'"'"', '"'"''"'"');
     ' | sudo -E tee /etc/yiimp/keys.php >/dev/null 2>&1
 
@@ -1133,6 +1124,23 @@
 
     define('"'"'YAAMP_USE_NGINX'"'"', true);
 
+    // Exchange public keys (private keys are in a separate config file)
+    define('"'"'EXCH_BINANCE_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_EXBITRON_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_HITBTC_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_KRAKEN_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_POLONIEX_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_SAFETRADE_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_YOBIT_KEY'"'"', '"'"''"'"');
+
+    define('"'"'EXCH_NESTEX_KEY'"'"', '"'"''"'"');
+	
     // Automatic withdraw to Yaamp btc wallet if btc balance > 0.3
     define('"'"'EXCH_AUTO_WITHDRAW'"'"', 0.3);
 
